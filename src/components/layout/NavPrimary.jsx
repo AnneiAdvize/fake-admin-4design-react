a47e7521d60e28d700079b974c0f989b523da4c7
AnneiAdvize <anne.pedro@iadvize.com>
2026-04-20 16:38:00 +0100

    logo

--- src/components/layout/NavPrimary.jsx
+++ src/components/layout/NavPrimary.jsx
@@ -24,7 +24,7 @@ export default function NavPrimary() {
     <nav className={styles.nav}>
       <div className={styles.inner}>
         <NavLink to="/home" className={styles.logo} aria-label="Dashboard home">
-          <img src="/assets/icons/logo.png" width="28" height="28" alt="" />
+          <img src="/assets/logo.png" width="28" height="28" alt="" />
         </NavLink>
 
         <div className={styles.items}>
